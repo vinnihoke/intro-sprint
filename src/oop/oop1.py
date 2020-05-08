ea6576c8e69adc7acd79e9ae918ec58c0812390b
Vinni Hoke <vinni.hoke@gmail.com>
2020-05-08 09:15:13 -0700

    OOP Completed, 1Q

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,41 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# BASE
+class Vehicle:
+    def __init__(self, name, desc):
+        self.name = name
+        self.desc = desc
+
+class GroundVehicle(Vehicle):
+    def __init__(self, name, desc, wheels, fuel):
+        super().__init__(name, desc)
+        self.wheels = wheels
+        self.fuel = fuel
+
+class Car(GroundVehicle):
+    def __init__(self, name, desc, wheels, fuel, bodytype):
+        super().__init__(name, desc, wheels, fuel)
+        self.bodystyle = bodystyle
+
+class Motorcycle(GroundVehicle):
+    def __init__(self, name, desc, wheels, fuel, purpose):
+        super().__init__(name, desc, fuel, wheels)
+        self.purpose = purpose
+
+class FlightVehicle(Vehicle):
+    def __init__(self, name, desc, purpose):
+        super().__init__(name, desc)
+        self.purpose = purpose
+
+class Airplane(FlightVehicle):
+    def __init__(self, name, desc, purpose, commuter, comfort):
+        super().__init__(name, desc, purpose)
+        self.commuter = commuter
+        self.comfort = comfort
+
+class Starship(FlightVehicle):
+    def __init__(self, name, desc, purpose, transport):
+        super().__init__(name, desc, purpose)
+        self.transport = transport
