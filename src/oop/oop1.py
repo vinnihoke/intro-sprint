92d9963c73f6753ee471147f14874acc8555a7d5
Vinni Hoke <vinni.hoke@gmail.com>
2020-05-08 11:00:27 -0700

    Completed sprint

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -19,39 +19,66 @@
 # Put a comment noting which class is the base class
 
 # BASE
+
+
 class Vehicle:
-    def __init__(self, name, desc):
-        self.name = name
-        self.desc = desc
+    def __init__(self):
+        # self.name = name
+        # self.desc = desc
+        pass
+
 
 class GroundVehicle(Vehicle):
-    def __init__(self, name, desc, wheels, fuel):
-        super().__init__(name, desc)
-        self.wheels = wheels
-        self.fuel = fuel
+    # def __init__(self, name, desc, wheels, fuel):
+    #     super().__init__(name, desc)
+    #     self.wheels = wheels
+    #     self.fuel = fuel
+    def __init__(self):
+        super().__init__()
+        pass
+
 
 class Car(GroundVehicle):
-    def __init__(self, name, desc, wheels, fuel, bodytype):
-        super().__init__(name, desc, wheels, fuel)
-        self.bodystyle = bodystyle
+    # def __init__(self, name, desc, wheels, fuel, bodytype):
+    #     super().__init__(name, desc, wheels, fuel)
+    #     self.bodytype = bodytype
+    def __init__(self):
+        super().__init__()
+        pass
+
 
 class Motorcycle(GroundVehicle):
-    def __init__(self, name, desc, wheels, fuel, purpose):
-        super().__init__(name, desc, fuel, wheels)
-        self.purpose = purpose
+    # def __init__(self, name, desc, wheels, fuel, purpose):
+    #     super().__init__(name, desc, fuel, wheels)
+    #     self.purpose = purpose
+    def __init__(self):
+        super().__init__()
+        pass
+
 
 class FlightVehicle(Vehicle):
-    def __init__(self, name, desc, purpose):
-        super().__init__(name, desc)
-        self.purpose = purpose
+    # def __init__(self, name, desc, purpose):
+    #     super().__init__(name, desc)
+    #     self.purpose = purpose
+    def __init__(self):
+        super().__init__()
+        pass
+
 
 class Airplane(FlightVehicle):
-    def __init__(self, name, desc, purpose, commuter, comfort):
-        super().__init__(name, desc, purpose)
-        self.commuter = commuter
-        self.comfort = comfort
+    # def __init__(self, name, desc, purpose, commuter, comfort):
+    #     super().__init__(name, desc, purpose)
+    #     self.commuter = commuter
+    #     self.comfort = comfort
+    def __init__(self):
+        super().__init__()
+        pass
+
 
 class Starship(FlightVehicle):
-    def __init__(self, name, desc, purpose, transport):
-        super().__init__(name, desc, purpose)
-        self.transport = transport
+    # def __init__(self, name, desc, purpose, transport):
+    #     super().__init__(name, desc, purpose)
+    #     self.transport = transport
+    def __init__(self):
+        super().__init__()
+        pass
--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -3,6 +3,7 @@
 # Also change it so the num_wheels defaults to 4 if not specified when the
 # object is constructed.
 
+
 class GroundVehicle():
     def __init__(self, num_wheels=None):
         self.num_wheels = 4 if num_wheels == None else num_wheels
@@ -23,11 +24,12 @@ class Motorcycle(GroundVehicle):
     def __init__(self, num_wheels=None):
         super().__init__(num_wheels)
         self.num_wheels = 2 if num_wheels == None else num_wheels
-    
+
     def drive(self):
         print("BRAAAP!!")
 
 # TODO
+
 
 vehicles = [
     GroundVehicle(),
